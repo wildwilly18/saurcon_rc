aa58f9f37898ab297e035d8f7dc35c408c5f0496
Bilson <wilson.lysford@gmail.com>
2025-04-18 05:48:32 -0500

    Initial commit of the Saurcon RC car drive controller code

---
+++ include/pins.h
@@ -0,0 +1,29 @@
+#ifndef PINS_H
+#define PINS_H
+
+// Servo Out Pin
+#define ESC_PIN 3
+
+
+
+
+//Pin for knob
+#define KNOB_IN A13
+
+//Pins for buttons
+#define BUTTON_ON 33
+#define BUTTON_OFF 34
+
+//Pins for hall sensor
+#define A_PIN 2
+#define B_PIN 1
+#define C_PIN 0
+
+//Pins to monitor current
+#define I_in_1 A9
+#define I_in_2 A8
+
+#define SCL_PIN 22
+#define SDA_PIN 21
+
+#endif // PINS_H
